bd8f94c96ca92657642a87d2a59d89b31d3dfa6e
FarhanGits <daffafarhan05@gmail.com>
2026-01-27 18:25:16 +0700

    refator trial: try to put controller logic to different layer (service)

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Services\CategoryServices;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
@@ -10,21 +11,13 @@ use Illuminate\Support\Str;
 
 class CategoryController extends Controller
 {
-    public function CreateCategory(Request $request) {
-        $input = $request->all();
-        $isAlreadyHave = Category::where('category_name', $input['category_name'])->exists();
-        $isUserCreated = Category::where('user_id', Auth::user()->user_id);
-
-        if ($isAlreadyHave && $isUserCreated) {
-            return redirect()->route('track-cashflow');
-        }
-        $data = [
-            'category_id' => 'CTGR-' . Str::ulid(),
-            'user_id' => Auth::user()->user_id,
-            'category_name' => $input['category_name'],
-            'category_type' => $input['category_type'],
-        ];
-        Category::create($data);
+    public function CreateCategory(Request $request, CategoryServices $service) {
+        $service->create(
+            category_id: 'CTGR-' . Str::ulid(),
+            user_id: Auth::user()->user_id,
+            category_name: $request->category_name,
+            category_type: $request->category_type
+        );
         return redirect()->route('track-cashflow');
     }
 }
