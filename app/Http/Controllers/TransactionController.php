d48eb8f94abba3062af9344340a62e39e6f0114a
FarhanGits <daffafarhan05@gmail.com>
2026-02-14 23:27:30 +0700

    changes: replace all transaction controller function business logic to service

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Enums\TransactionMethod;
 use App\Models\Category;
 use App\Models\Transaction;
+use App\Services\TransactionService;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -17,90 +18,41 @@ use function Illuminate\Support\now;
 
 class TransactionController extends Controller
 {
-    public function ViewTransactionPage(Request $request): Response {
-        $user_id = Auth::user()->user_id;
-        $categories = Category::all();
-        $transactions = Transaction::with('category')->where('user_id', $user_id)->orderBy('transaction_date', 'desc')->get();
+    public function ViewTransactionPage(TransactionService $service): Response {
+        [$user_id, $categories, $transactions, $transaction_methods] = $service->viewPage();
         
-        // Enums
-        $transaction_methods = TransactionMethod::values();
-        return Inertia::render('track-cashflow', compact('user_id', 'categories', 'transactions', 'transaction_methods'));
+        return Inertia::render('track-cashflow', [
+            'user_id' => $user_id,
+            'categories' => $categories,
+            'transactions' => $transactions,
+            'transaction_methods' => $transaction_methods,
+        ]);
     }
 
-    public function CreateTransaction(Request $request) {
+    public function CreateTransaction(Request $request, TransactionService $service) {
         $input = $request->all();
-        $data = [
-            'transaction_id' => 'TRSC-' . Str::ulid(),
-            'user_id' => Auth::user()->user_id,
-            'category_id' => $input['category_id'],
-            'transaction_date' => $input['transaction_date'],
-            'transaction_amount' => $input['transaction_amount'],
-            'transaction_type' => $input['transaction_type'],
-            'transaction_method' => $input['transaction_method'],
-            'transaction_note' => $input['transaction_note']
-        ];
-        Transaction::create($data);
+        
+        $service->create($input);
+        
         return redirect()->route('track-cashflow');
     }
 
-    public function ViewTransactionList (Request $request) {
-        $user_id = Auth::user()->user_id;
-
-        $query = Transaction::with('category')->where('user_id', $user_id);
-        
+    public function ViewTransactionList (Request $request, TransactionService $service) {
         $timeframe = $request->query('timeframe', 'mtd');
-        if ($timeframe === 'mtd') {
-            $query->whereBetween('transaction_date', [
-                now()->startOfMonth(),
-                now()->endOfMonth()
-            ]);
-        } elseif ($timeframe === 'ytd') {
-            $query->whereBetween('transaction_date', [
-                now()->startOfYear(),
-                now()->endOfYear()
-            ]);
-        } elseif ($timeframe === '30d') {
-            $month = Carbon::now()->subDays(30);
-            $query->where('transaction_date', ">=", $month);
-        }
 
-        // $transactions = $query->paginate(10);
-        $transactions = $query->orderBy('transaction_date', 'desc')->get();
+        $transactions = $service->viewList($timeframe);
+
         return Inertia::render('cashflow', [
             'transactions' => $transactions,
             'filters' => $request->only(['timeframe'])
         ]);
     }
     
-    public function showPDF(Request $request) {
-        $user_id = Auth::user()->user_id;
-    
-        $query = Transaction::with('category')->where('user_id', $user_id);
-        
+    public function showPDF(Request $request, TransactionService $service) {
         $timeframe = $request->query('timeframe', 'mtd');
-        if ($timeframe === 'mtd') {
-            $query->whereBetween('transaction_date', [
-                now()->startOfMonth(),
-                now()->endOfMonth()
-            ]);
-        } elseif ($timeframe === 'ytd') {
-            $query->whereBetween('transaction_date', [
-                now()->startOfYear(),
-                now()->endOfYear()
-            ]);
-        } elseif ($timeframe === '30d') {
-            $month = Carbon::now()->subDays(30);
-            $query->where('transaction_date', ">=", $month);
-        }
 
-        $transactions = $query->orderBy('transaction_date', 'desc')->get();
-
-        $pdf = Pdf::loadView('transaction-report-pdf', [
-            'transactions' => $transactions
-
-        ])->setPaper('A4', 'portrait');
-
-        $pdfname = 'Report Bulan Mei';
+        [$pdf, $pdfname] = $service->showPDF($timeframe);
+        
         return $pdf->stream($pdfname . '.pdf');
     }
 }
--- app/Services/TransactionService.php
+++ app/Services/TransactionService.php
@@ -2,12 +2,99 @@
 
 namespace App\Services;
 
+use App\Enums\TransactionMethod;
+use App\Http\Resources\TransactionResource;
+use App\Models\Category;
 use App\Models\Transaction;
+use Barryvdh\DomPDF\Facade\Pdf;
+use Carbon\Carbon;
 use DomainException;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
 
 class TransactionService
 {
-    public function create(string $transaction_id, string $user_id, string $category_id, string $transaction_date, int $transaction_amount, string $transaction_type, string $transaction_method, string $transaction_note)
-    {}
+    public function viewPage()
+    {
+        $user_id = Auth::user()->user_id;
+        $categories = Category::all();
+        $transactions = Transaction::with('category')->where('user_id', $user_id)->orderBy('transaction_date', 'desc')->limit(10)->get();
+        
+        // Enums
+        $transaction_methods = TransactionMethod::values();
+        
+        return [
+            $user_id,
+            $categories,
+            $transactions,
+            $transaction_methods
+        ];
+    }
+    public function create(array $input)
+    {
+        $input['transaction_id'] = 'TRSC-' . Str::ulid();
+        $input['user_id'] = Auth::user()->user_id;
+        
+        Transaction::create($input);
+    }
+
+    public function viewList(array|string|null $timeframe)
+    {
+        $user_id = Auth::user()->user_id;
+
+        $query = Transaction::with('category')->where('user_id', $user_id);
+        
+        if ($timeframe === 'mtd') {
+            $query->whereBetween('transaction_date', [
+                now()->startOfMonth(),
+                now()->endOfMonth()
+            ]);
+        } elseif ($timeframe === 'ytd') {
+            $query->whereBetween('transaction_date', [
+                now()->startOfYear(),
+                now()->endOfYear()
+            ]);
+        } elseif ($timeframe === '30d') {
+            $month = Carbon::now()->subDays(30);
+            $query->where('transaction_date', ">=", $month);
+        }
+
+        // $transactions = $query->paginate(10);
+        $transactions = $query->orderBy('transaction_date', 'desc')->paginate(10);
+
+        return TransactionResource::collection($transactions);
+    }
+
+    public function showPDF(array|string|null $timeframe)
+    {
+        $user_id = Auth::user()->user_id;
+    
+        $query = Transaction::with('category')->where('user_id', $user_id);
+        
+        if ($timeframe === 'mtd') {
+            $query->whereBetween('transaction_date', [
+                now()->startOfMonth(),
+                now()->endOfMonth()
+            ]);
+        } elseif ($timeframe === 'ytd') {
+            $query->whereBetween('transaction_date', [
+                now()->startOfYear(),
+                now()->endOfYear()
+            ]);
+        } elseif ($timeframe === '30d') {
+            $month = Carbon::now()->subDays(30);
+            $query->where('transaction_date', ">=", $month);
+        }
+
+        $transactions = $query->orderBy('transaction_date', 'desc')->get();
+
+        $pdf = Pdf::loadView('transaction-report-pdf', [
+            'transactions' => $transactions
+
+        ])->setPaper('A4', 'portrait');
+
+        $pdfname = 'Report Bulan Mei';
+
+        return [$pdf, $pdfname];
+    }
 }
